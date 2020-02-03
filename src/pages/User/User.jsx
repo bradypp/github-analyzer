b3e0990fb8cd47405de54003c6a0af3170e1d510
Paul Brady <53856442+bradypp@users.noreply.github.com>
2020-02-03 23:18:43 +0000

    Updated the user page

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,19 +1,52 @@
-import React, { useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
+import PropTypes from 'prop-types';
 
 import { GithubContext } from 'context/github';
 
 import './User.scss';
 
-const User = () => {
+const User = ({ match }) => {
     const githubContext = useContext(GithubContext);
-    const { user, loading } = githubContext;
+    const { user, loading, getUser } = githubContext;
+
+    useEffect(() => {
+        getUser(match.params.login);
+    }, [getUser, match.params.login]);
+
+    const {
+        name,
+        company,
+        avatar_url,
+        location,
+        bio,
+        blog,
+        login,
+        html_url,
+        followers,
+        following,
+        public_repos,
+        public_gists,
+        hireable,
+    } = user;
+
     return (
         <div className="container">
-            <div className="user-info">
-                <h1>User Info</h1>
-            </div>
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <div className="user-info">
+                    <h1>User Info</h1>
+                    <p>{name}</p>
+                </div>
+            )}
         </div>
     );
+};
+
+User.propTypes = {
+    match: PropTypes.shape({
+        params: PropTypes.objectOf(PropTypes.string),
+    }).isRequired,
 };
 
 export default User;

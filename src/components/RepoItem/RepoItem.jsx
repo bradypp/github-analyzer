62380b7590d358fc143487ebd3fc6342ab09877f
bradypp <bradypp44@gmail.com>
2020-02-22 17:32:11 +0000

    Updated repo item size and description to prevent overflowing text

--- src/components/RepoItem/RepoItem.jsx
+++ src/components/RepoItem/RepoItem.jsx
@@ -35,6 +35,19 @@ const RepoItem = ({
         return `${size} KB`;
     };
 
+    const getDescription = () => {
+        const descriptionChars = description.split('');
+        const descriptionLength = 140;
+        if (descriptionChars.length > descriptionLength) {
+            const newDescription = descriptionChars.slice(0, descriptionLength);
+            if (newDescription[descriptionLength - 1].match(/\W$/gm)) {
+                newDescription.pop();
+            }
+            return `${newDescription.join('').trim()}...`;
+        }
+        return description.trim();
+    };
+
     return (
         <a href={html_url} target="_blank" rel="noopener noreferrer" className="repo-github-url">
             <li className="repo-item">
@@ -42,13 +55,10 @@ const RepoItem = ({
                     <Octicon icon={Repo} />
                     <h3>{name}</h3>
                 </div>
-                {description && <p className="repo-item__description">{description}</p>}
+                {description && <p className="repo-item__description">{getDescription()}</p>}
                 <div className="repo-item__attributes repo-item__attributes--1">
                     <div className="repo-item__attributes__created_at">
-                        <Octicon
-                            icon={Calendar}
-                            size="small"
-                        />
+                        <Octicon icon={Calendar} size="small" />
                         {new Date(created_at).toLocaleDateString('en-UK', {
                             day: 'numeric',
                             month: 'numeric',
@@ -56,10 +66,7 @@ const RepoItem = ({
                         })}
                     </div>
                     <div className="repo-item__attributes__updated_at">
-                        <Octicon
-                            icon={Sync}
-                            size="small"
-                        />
+                        <Octicon icon={Sync} size="small" />
                         {new Date(updated_at).toLocaleDateString('en-UK', {
                             day: 'numeric',
                             month: 'numeric',
@@ -69,38 +76,23 @@ const RepoItem = ({
                 </div>
                 <div className="repo-item__attributes repo-item__attributes--2">
                     <div className="repo-item__attributes__language">
-                        <Octicon
-                            icon={Browser}
-                            size="small"
-                        />
+                        <Octicon icon={Browser} size="small" />
                         {language}
                     </div>
                     <div className="repo-item__attributes__stars">
-                        <Octicon
-                            icon={Star}
-                            size="small"
-                        />
+                        <Octicon icon={Star} size="small" />
                         {stargazers_count}
                     </div>
                     <div className="repo-item__attributes__forks">
-                        <Octicon
-                            icon={RepoForked}
-                            size="small"
-                        />
+                        <Octicon icon={RepoForked} size="small" />
                         {forks_count}
                     </div>
                     <div className="repo-item__attributes__issues">
-                        <Octicon
-                            icon={IssueOpened}
-                            size="small"
-                        />
+                        <Octicon icon={IssueOpened} size="small" />
                         {open_issues_count}
                     </div>
                     <div className="repo-item__attributes__watching">
-                        <Octicon
-                            icon={Eye}
-                            size="small"
-                        />
+                        <Octicon icon={Eye} size="small" />
                         {watchers_count}
                     </div>
                     <div className="repo-item__attributes__size">{calculateSize()} </div>

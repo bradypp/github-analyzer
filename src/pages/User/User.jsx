df52c30ef1bdf2467b612bb9baea491e8b819d0c
bradypp <bradypp44@gmail.com>
2020-02-23 14:33:19 +0000

    Added github repo corner button

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { UserInfo, Repos, Stats } from 'components';
+import { UserInfo, Repos, Stats, Header } from 'components';
 import './UserStyles.scss';
 
 const User = () => (
     <>
+        <Header />
         <div className="user user__section-1">
             <UserInfo />
             <Stats />

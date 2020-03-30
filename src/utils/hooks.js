aa218cd4c637cf567876f3c21abc1f7a7b2af340
bradypp <bradypp44@gmail.com>
2020-03-30 22:20:53 +0100

    Added esc close to useClickedOutsideHandler

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -3,6 +3,11 @@ import { useEffect } from 'react';
 // Hook that alerts clicks outside of the passed ref
 export const useClickedOutsideHandler = (ref, condition, handler, handlerParams = null) => {
     // Alert if clicked outside of element
+    const handleKeyDown = event => {
+        if (ref.current && condition && event.keyCode === 27) {
+            handler(handlerParams);
+        }
+    };
     const handleClickOutside = event => {
         if (ref.current && !ref.current.contains(event.target) && condition) {
             handler(handlerParams);
@@ -12,9 +17,11 @@ export const useClickedOutsideHandler = (ref, condition, handler, handlerParams 
     useEffect(() => {
         // Bind the event listener
         document.addEventListener('mousedown', handleClickOutside);
+        document.addEventListener('keydown', handleKeyDown);
         return () => {
             // Unbind the event listener on clean up
             document.removeEventListener('mousedown', handleClickOutside);
+            document.removeEventListener('keydown', handleKeyDown);
         };
     });
 };
